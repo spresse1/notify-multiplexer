2fb336014c28e21c697318e4603a2d9cf53f7205
Steven Presser <steve@pressers.name>
2012-07-16 01:14:41 -0400

    Added the ability to make & install a python egg via make_egg.py

---
+++ libnotifymultiplex/__init__.py
@@ -0,0 +1,1 @@
+from libnotifymultiplex.libnotifymultiplex import *
---
+++ make_egg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='libnotifymultiplex',
+    version='0.1',
+    description='Library to assist in distributed (multiplexed) notifications',
+    author='spresse1',
+    author_email='public@pressers.name',
+    url='',
+    packages=['libnotifymultiplex'])
