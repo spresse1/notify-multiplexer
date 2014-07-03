4f8d88baaf1747f579c7ec893bbef47d0d90be02
Steven Presser <steve@pressers.name>
2014-07-03 01:31:52 -0400

    Remove gunk/obsolete stuff

--- sink/android/NotifyMultiplexerAndroid/gen/name/pressers/notifymultiplexer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package name.pressers.notifymultiplexer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- sink/test-unicast-client.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import socket
-
-sock = socket.socket(socket.AF_UNIX,socket.SOCK_DGRAM)
-sock.connect("\0notify-multiplexer")
-sock.send("ten\0tests\0\0\0wen\0wests\0im\0\0")
-#sock.send("twenty\ntests\n")
-#sock.send("ten\ntests\n")
