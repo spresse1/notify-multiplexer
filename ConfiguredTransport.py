235063c0795be92a51d04ec7bec03fbeebe68016
spresse1 <steve@pressers.name>
2014-11-22 19:38:05 -0500

    Decided that I should have something less complicated than broadcasting to start.

---
+++ ConfiguredTransport.py
@@ -0,0 +1,16 @@
+class ConfiguredTransport:
+	"""An implemetation of the transport layer for the NetworkBus class, using direct TCP/TLS connections to a pre-specified set of other servers."""
+	def __init__():
+		pass
+		
+	def connect():
+		pass
+	
+	def disconnect():
+		pass
+	
+	def send():
+		pass
+	
+	def receive():
+		pass
